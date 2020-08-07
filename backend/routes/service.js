7756a4f9b93e958641f7673bbbe89f56ce1258ec
Arjan Singh Bal <arjan3141@gmail.com>
2020-08-08 00:30:40 +0530

    create service and send to db

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const { models } = require("../sequelize");
 const extractFiles = require("../middlewares/files");
+const files = require("../middlewares/files");
 
 const router = express.Router();
 
-router.post("", extractFiles, (req, res) => {
-  // console.log(req.body);
-  // console.log(req.files);
+// {TODO: add exception handling}
+router.post("", extractFiles, async (req, res) => {
+  const { body, files } = req;
+
+  const newService = await models.service.create({
+    category: body.category,
+    name: body.name,
+    estimateUnit: body.estimateUnit,
+    priceEstimate: body.priceEstimate,
+    description: body.description,
+    contact: body.contactNumber,
+  });
+
+  locations = body.locations.split(",");
+  // add all the locations
+  // associate service with all the locations
+  for (let i = 0; i < locations.length; ++i) {
+    let loc = await models.location.findByPk(locations[i]);
+    // create the location if it's not already present
+    if (!loc) {
+      loc = await models.location.create({ name: locations[i] });
+    }
+    newService.addLocation(loc);
+  }
+
+  // associate all services with the image urls
+  for (let i = 0; i < files.length; ++i) {
+    await newService.createServiceImage({ url: files[i].path });
+  }
+
   res.status(200).json({
     message: "Service Created",
   });
