2f9cbb94dc6d5de443357cf3c5f0093e2df90b94
Arjan Singh Bal <arjan3141@gmail.com>
2020-08-14 00:42:05 +0530

    add carousel

--- frontend/src/components/products/ImageCarousel.jsx
+++ frontend/src/components/products/ImageCarousel.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
-import Carousel from "react-responsive-carousel";
+import { Carousel } from "react-responsive-carousel";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ImageCarousel = ({ images }) => {
   return (
     <Fragment>
-      <div>
+      <Carousel showArrows={true}>
         {images.map((image) => (
           <img
             key={image.url}
@@ -12,7 +14,7 @@ const ImageCarousel = ({ images }) => {
             alt={image.url}
           />
         ))}
-      </div>
+      </Carousel>
     </Fragment>
   );
 };
