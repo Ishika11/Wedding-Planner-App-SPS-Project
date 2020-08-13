f949d0dd371a3488ae338b9fccc0a119373349a0
Sreeramji K S <jamji1729@gmail.com>
2020-08-13 22:57:52 +0530

    Add newline at EOF

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,65 +1,63 @@
-import React from 'react';
-import Auth from '../auth/Auth.js';
+import React from "react";
+import Auth from "../auth/Auth.js";
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import AddBox from '@material-ui/icons/AddBox';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+import AddBox from "@material-ui/icons/AddBox";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-        fontFamily: 'Kalam, cursive',
-    },
-    button: {
-        margin: theme.spacing(1),
-        fontFamily: 'Averia Sans Libre, cursive',
-    },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: "Kalam, cursive",
+  },
+  button: {
+    margin: theme.spacing(1),
+    fontFamily: "Averia Sans Libre, cursive",
+  },
 }));
 
-
 export default function Navbar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h4" className={classes.title}>
-                        EaseMyशादी
-                    </Typography>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" className={classes.title}>
+            EaseMyशादी
+          </Typography>
 
-                    <Button
-                        color="inherit"
-                        className={classes.button}
-                        startIcon={<AddBox />}
-                        size="x-large"
-                    >
-                        Services
-                    </Button>
+          <Button
+            color="inherit"
+            className={classes.button}
+            startIcon={<AddBox />}
+            size="x-large"
+          >
+            Services
+          </Button>
 
-                    <Button
-                        color="inherit"
-                        className={classes.button}
-                        startIcon={<ShoppingCart />}
-                        size="x-large"
-                    >
-                        Cart
-                    </Button>
+          <Button
+            color="inherit"
+            className={classes.button}
+            startIcon={<ShoppingCart />}
+            size="x-large"
+          >
+            Cart
+          </Button>
 
-                    <Auth />
-
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+          <Auth />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
