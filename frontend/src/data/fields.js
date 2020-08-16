ff348bf28b6c86666f891339594d1334d5b6b2fa
shejal gupta <shejalgupta@shejals-MacBook-Pro.local>
2020-08-16 19:43:08 +0530

    add various single select filters with data list in tabular form

---
+++ frontend/src/data/fields.js
@@ -0,0 +1,24 @@
+export const REVIEWS = [
+    "1 star & up",
+    "2 star & up",
+    "3 star & up",
+    "4 star & up",
+    "5 star & up",
+  ];
+  
+  export const LOCATIONS = [
+    "Delhi",
+    "Mumbai",
+    "Bangalore",
+    "Kolkata",
+    "Chandigarh",
+    "Goa",
+  ];
+
+  export const PRICES = [
+    "Under 5Lakh",
+    "5Lakh to 10Lakh ",
+    "10Lakh to 25Lakh",
+    "25Lakh to 50Lakh",
+    "Above 50Lakh",
+  ];
