b42181e85714f17157b41eac9d8361f3af55a93c
Arjan Singh Bal <arjan3141@gmail.com>
2020-08-19 13:23:15 +0530

    added tokens to outgoing requests

---
+++ frontend/src/utils/setAuthToken.js
@@ -0,0 +1,9 @@
+import axios from "../actions/axios";
+
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
