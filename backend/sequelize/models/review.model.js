0b8fd5dce35036e48ec4a1b4131364c8d36ca575
Arjan Singh Bal <arjan3141@gmail.com>
2020-08-25 23:27:42 +0530

    part of previous commit

--- backend/sequelize/models/review.model.js
+++ backend/sequelize/models/review.model.js
@@ -11,7 +11,7 @@ module.exports = (db) => {
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     creator_email: {
